a43fe879e88a1ad611cf0737e81b080678253c50
startted <artem@startted.com>
2021-04-01 16:33:34 +0300

    - remove .env

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,12 @@ cd /var/www/$SITENAME/data/www/$SITENAME;
 
 cd /var/www/$SITENAME/data/www/$SITENAME;
 git pull;
-composer install;
+composer install --ignore-platform-reqs=php;
 php artisan migrate;
 php artisan config:cache;
 php artisan route:cache;
 php artisan view:cache;
+NVM_DIR="$HOME/.nvm"
+nvm use 12.14.1
 yarn;
 yarn production;
