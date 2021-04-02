4b8a01a2b186d84c7b173adae9f3474d9c9e6cc3
TheFatRat <Generation_2020>
2021-04-02 18:30:32 +0300

    trying to croppa

---
+++ resources/js/assets/icons/IconTurn.vue
@@ -0,0 +1,27 @@
+<template>
+    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_b)">
+            <rect width="48" height="48" rx="24" fill="black" fill-opacity="0.24"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M24 13C20.9868 13 18.1859 14.2223 16.1568 16.2859L15.9978 13.9326L14.0023 14.0674L14.4032 20H20V18H17.2905C18.9685 16.1232 21.3861 15 24 15C27.2509 15 30.198 16.7372 31.7966 19.5007L33.5278 18.4993C31.5753 15.1239 27.9722 13 24 13ZM32.4853 26L24 34.4853L15.5148 26L24 17.5147L32.4853 26ZM18.3432 26L24 31.6569L29.6569 26L24 20.3431L18.3432 26Z" fill="white"/>
+        </g>
+        <defs>
+            <filter id="filter0_b" x="-4" y="-4" width="56" height="56" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feGaussianBlur in="BackgroundImage" stdDeviation="2"/>
+                <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur" result="shape"/>
+            </filter>
+        </defs>
+    </svg>
+
+</template>
+
+<script>
+export default {
+    name: "IconTurn"
+}
+</script>
+
+<style scoped>
+
+</style>
