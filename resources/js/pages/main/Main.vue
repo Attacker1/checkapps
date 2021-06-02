8bd3b2fb6be09b9385b616404ba1d59ff824ce03
TheFatRat <Generation_2020>
2021-06-02 18:04:53 +0300

    search users, fixes

--- resources/js/pages/main/Main.vue
+++ resources/js/pages/main/Main.vue
@@ -7,54 +7,52 @@
 </template>
 
 <script>
-import CheckView from '@/components/check/CheckView';
-import CheckViewSkeleton from '@/components/skeleton/CheckViewSkeleton';
-import {mapActions, mapGetters} from 'vuex';
+    import CheckView from '@/components/check/CheckView';
+    import CheckViewSkeleton from '@/components/skeleton/CheckViewSkeleton';
+    import {mapActions, mapGetters} from 'vuex';
 
-export default {
-    name: 'Main',
-    components: {CheckView, CheckViewSkeleton},
-    methods: {
-        ...mapActions({
-            'checkExpiryDate': 'checks/checkExpiryTime',
-            'fetchChecks': 'checks/fetchChecks',
-        })
-    },
-    computed: {
-        ...mapGetters({
-            check: 'currentCheck/currentCheck',
-            loader: 'common/loader'
-        })
-    },
-    async beforeMount() {
-        await this.$recaptchaLoaded()
-
-        const token = await this.$recaptcha('check')
-        // console.log(token);
-        this.fetchChecks({recaptcha_token: token})
-    },
-    mounted() {
-        setInterval(() => {
-            this.checkExpiryDate();
-        }, 1000)
+    export default {
+        name: 'Main',
+        components: {CheckView, CheckViewSkeleton},
+        methods: {
+            ...mapActions({
+                'checkExpiryDate': 'checks/checkExpiryTime',
+                'fetchChecks': 'checks/fetchChecks',
+            }),
+        },
+        computed: {
+            ...mapGetters({
+                check: 'currentCheck/currentCheck',
+                loader: 'common/loader'
+            }),
+        },
+        async beforeMount() {
+            await this.$recaptchaLoaded()
+            const token = await this.$recaptcha('check')
+            this.fetchChecks({recaptcha_token: token})
+        },
+        mounted() {
+            setInterval(() => {
+                this.checkExpiryDate();
+            }, 1000)
+        },
     }
-}
 </script>
 
 <style lang="scss" scoped>
-.main-page {
-    min-height: calc(100vh - 91px);
-    display: flex;
-    align-items: center;
+    .main-page {
+        min-height: calc(100vh - 91px);
+        display: flex;
+        align-items: center;
 
-    &__notfound {
-        text-align: center;
-        width: 100%;
-        font-size: 72px;
+        &__notfound {
+            text-align: center;
+            width: 100%;
+            font-size: 72px;
 
-        @media screen and (max-width: 767px) {
-            font-size: 40px;
+            @media screen and (max-width: 767px) {
+                font-size: 40px;
+            }
         }
     }
-}
 </style>
