1c34cee9897b2010a2381cf32791a03faba157a8
startted <artem@startted.com>
2021-06-08 18:20:17 +0300

    fix git

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command(function () {
+             Log::error('Выполнено по расписанию');
+         })->everyMinute();
     }
 
     /**
