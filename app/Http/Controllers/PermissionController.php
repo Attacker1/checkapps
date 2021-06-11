66fbdfb91ce96aac8124951de4f4bab403ca9d67
Nikita Malkin <demeja16@gmail.com>
2021-06-11 12:27:55 +0300

    add swagger

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -13,6 +13,39 @@ class PermissionController extends Controller
         $this->permissionService = $permissionService;
     }
 
+    /**
+     * @OA\GET(
+     *     path="/api/permissions",
+     *     summary="Позволяет получить все разрешения",
+     *     description="Позволяет получить все разрешения",
+     *     operationId="permissions",
+     *     tags={"Permissions"},
+     *     security={ {"passport": {} }},
+     *     @OA\Response(
+     *         response=401,
+     *         description="Вылетает если запрос не авторизован",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Вылетает если у пользователя не прав для этого метода",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="error", type="string"),
+     *             @OA\Property(property="code", type="string"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successfully response",
+     *         @OA\JsonContent(
+     *                  @OA\Property(property="slug", type="string"),
+     *                  @OA\Property(property="name", type="string"),
+     *         ),
+     *     ),
+     * )
+     */
     public function permissions() {
         $response = $this->permissionService->permissions();
 
